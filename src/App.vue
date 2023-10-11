14b2b1c29a4b12ab9c5e8fdfa90fc8458b6c2edf
Buhito <luisj.sanz.sardina@gmail.com>
2023-10-12 01:28:10 +0200

    Create structure of Emotaku mainContent

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,25 @@
   <BackgroundDayNightEmotaku></BackgroundDayNightEmotaku>
   <EmotakuLines></EmotakuLines>
   <BannerEmotaku></BannerEmotaku>
-  <div class="home">
-    <aside class="leftSide"></aside>
-      <div class="selfExplanation">
-        <marquee>Hola chaval, eres una rata</marquee>
-      </div>
-      <div class="mainTopRightInfo">
-        <div class="imOnline"></div>
-        <div class="zones"></div>
-      </div>
-    
-  </div>
+  <main class="home">
+    <div class="mainContent">
+      <aside class="mainLeft">
+        <nav class="principalViews">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <router-link :to="{ name: 'testingEverything' }">TestingZone</router-link>
+          <router-link :to="{ name: 'tricksBoy' }">TricksBoy</router-link>
+        </nav>
+      </aside>
+      <aside class="mainCenter">
+        <marquee behavior="scroll" direction="left">Bienvenid@ a 🖤Emotaku🤍 pequeña ratita</marquee>
+        <router-view />
+      </aside>
+      <aside class="mainRight"></aside>
+    </div>
 
-  <router-view />
-<nav>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link> |
-  <router-link :to="{ name: 'testingEverything' }"><abbr>TestingZone</abbr></router-link> |
-  <router-link :to="{ name: 'tricksBoy' }">TricksBoy</router-link> | 
-</nav>
+
+  </main>
 </template>
 
 
@@ -32,12 +32,12 @@ import EmotakuLines from '@/components/EmotakuLines.vue';
 import BannerEmotaku from '@/components/BannerEmotaku.vue';
 export default defineComponent({
 
-components: {
-BackgroundDayNightEmotaku,
-EmotakuLines,
-BannerEmotaku
+  components: {
+    BackgroundDayNightEmotaku,
+    EmotakuLines,
+    BannerEmotaku
 
-},
+  },
 
 });
 </script>
@@ -47,29 +47,59 @@ BannerEmotaku
 @import './assets/styles/fonts.css';
 
 #app {
-font-family: Pixel Operator, Avenir, Helvetica, Arial, sans-serif;
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
-text-align: center;
-color: #2c3e50;
+  font-family: Pixel Operator, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 
 nav {
-padding: 30px;
+  padding: 30px;
 }
 
 nav a {
-font-weight: bold;
-color: #2c3e50;
+  font-weight: bold;
+  color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-color: #42b983;
+  color: #42b983;
 }
 
 .home {
+  width: 100%;
+  height: 85vh;
+  outline: var(--outlineContent);
+  margin-bottom: 20px;
+}
 
+.mainContent {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+}
+
+.mainLeft {
+  width: 25%;
+  height: 100%;
+  background-color: red;
+}
+
+.mainRight {
+  width: 25%;
+  height: 100%;
+  background-color: blue;
+}
+
+.mainCenter {
+  overflow-x: hidden;
+  width: 50%;
+  height: 100%;
+  background-color: black;
 }
 
 .mainTopInfo {
@@ -82,6 +112,11 @@ color: #42b983;
   background-color: red;
   border: 2px solid black;
 
+}
+
+.principalViews {
+  display: flex;
+  flex-direction: column;
 }
 
 .selfExplanation {
@@ -101,4 +136,12 @@ color: #42b983;
   background-color: blue;
   margin-left: 28px;
 }
-</style>
+
+@media (max-width: 1110px) {
+
+  .mainLeft,
+  .mainRight,
+  .mainCenter {
+    width: 100%;
+  }
+}</style>
