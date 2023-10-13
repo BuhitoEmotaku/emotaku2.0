94e7940f8ec1f7e31b605a00ef9cbb9f588f517d
Buhito <luisj.sanz.sardina@gmail.com>
2023-10-14 00:21:35 +0200

    footer Emotaku

---
+++ src/components/FooterEmotaku.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer>
+      <aside>
+        <marquee class="footerAuthor">coded by buhito with love🖤 2023-2023</marquee>
+      </aside>
+    </footer>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'FooterEmotaku',
+
+  });
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+
+footer {
+    background-color: black;
+    color: green;
+    margin-bottom: 30px;
+  }
+ .footerAuthor {
+  font-weight: bold;
+  
+  width: 50%;
+  padding: 3px;
+ }
+  </style>
+  
